f7082a863f3b30a21fd9454ce3d713c64b2441f0
pangil5634 <oksk6681@naver.com>
2026-01-21 00:17:35 +0900

    refactor : move student modal to new folder
    기존의 학생 모달에 대한 파일들을 modal 폴더를 생성하여 분리하였습니다.

--- src/pages/student/index.tsx
+++ src/pages/student/index.tsx
@@ -1,7 +1,7 @@
 import { useStudentModalStore } from '@/store/studentModalStore';
 import ButtonSection from './components/ButtonSection';
 import TableSection from './components/TableSection';
-import StudentModal from './components/StudentModal';
+import StudentModal from './components/modal/StudentModal';
 
 function StudentPage() {
   const isOpen = useStudentModalStore((state) => state.isOpen);

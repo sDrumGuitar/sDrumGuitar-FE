f33d63edae670d80287e8b18630fa5e755391d65
pangil5634 <oksk6681@naver.com>
2026-01-23 00:02:26 +0900

    feat : create courseModalStore
    수강 모달에 대한 상태를 추가했습니다.
    
    - 이 부분은 이후 제네릭으로 개선될 수 있습니다.
    - studentModalStore.ts와 같이.

--- src/pages/course/index.tsx
+++ src/pages/course/index.tsx
@@ -1,7 +1,11 @@
+import ModalOpenButton from '@/shared/modal/ModalOpenButton';
+import { useCourseModalStore } from '@/store/courseModalStore';
+
 function CoursePage() {
+  const openCreate = useCourseModalStore((state) => state.openCreate);
   return (
     <div>
-      <p>Course Page</p>
+      <ModalOpenButton text="신규수업 추가" openModal={openCreate} />
     </div>
   );
 }
---
+++ src/store/courseModalStore.ts
@@ -0,0 +1,49 @@
+import { create } from 'zustand';
+import type { Course } from '@/types/course';
+
+type ModalMode = 'CREATE' | 'DETAIL' | 'UPDATE' | null;
+
+interface CourseModalState {
+  isOpen: boolean;
+  mode: ModalMode;
+  selectedCourse: Course | null;
+
+  openCreate: () => void;
+  openDetail: (course: Course) => void;
+  openUpdate: (course: Course) => void;
+  close: () => void;
+}
+
+export const useCourseModalStore = create<CourseModalState>((set) => ({
+  isOpen: false,
+  mode: null,
+  selectedCourse: null,
+
+  openCreate: () =>
+    set({
+      isOpen: true,
+      mode: 'CREATE',
+      selectedCourse: null,
+    }),
+
+  openDetail: (course) =>
+    set({
+      isOpen: true,
+      mode: 'DETAIL',
+      selectedCourse: course,
+    }),
+
+  openUpdate: (course) =>
+    set({
+      isOpen: true,
+      mode: 'UPDATE',
+      selectedCourse: course,
+    }),
+
+  close: () =>
+    set({
+      isOpen: false,
+      mode: null,
+      selectedCourse: null,
+    }),
+}));

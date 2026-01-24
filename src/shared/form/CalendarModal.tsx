56dd0e1671eadc11e036ea3165804e48cbf4147e
pangil5634 <oksk6681@naver.com>
2026-01-25 06:32:21 +0900

    feat : implement date choice modal
    날짜 선택을 위한 모달을 구현했습니다.
    - react-calendar를 사용했습니다.
    - 전역 상태를 추가하여 구현했습니다.

---
+++ src/shared/form/CalendarModal.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import './CalendarModal.css';
+import { formatKoreanDate } from '@/utils/formDate';
+import { useDateModalStore } from '@/store/dateModalStore';
+import ModalWrapper from '../modal/ModalWrapper';
+import NormalButton from '../button/NormalButton';
+
+export type DatePiece = Date | null;
+export type SelectedDate = DatePiece | [DatePiece, DatePiece];
+
+function CalendarModal() {
+  const [originSelectedDate, setOriginSelectedDate] = useState<SelectedDate>(
+    new Date(),
+  );
+  const { setSelectedDate, close } = useDateModalStore();
+
+  const choiceDate = () => {
+    const date = originSelectedDate?.toString();
+    if (date) {
+      setSelectedDate(date);
+    }
+
+    close();
+  };
+
+  return (
+    <ModalWrapper onClose={close}>
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-lg font-bold">날짜 선택</h2>
+        <button onClick={close}>닫기</button>
+      </div>
+      <Calendar
+        onChange={setOriginSelectedDate}
+        value={originSelectedDate}
+        calendarType="gregory"
+        prev2Label={null}
+        next2Label={null}
+      />
+      <div className="flex justify-between mt-10">
+        <p>
+          <span>선택된 날짜 : </span>
+
+          <span>{formatKoreanDate(originSelectedDate?.toString() || '')}</span>
+        </p>
+        <NormalButton onClick={choiceDate} text="저장" />
+      </div>
+    </ModalWrapper>
+  );
+}
+export default CalendarModal;

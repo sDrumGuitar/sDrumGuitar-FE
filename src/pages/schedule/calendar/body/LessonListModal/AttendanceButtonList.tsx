91ab94b4b106fe0872feacc6e7778100f4959299
pangil5634 <oksk6681@naver.com>
2026-02-09 23:17:01 +0900

    feat : add attendance status's save button
    회차 정보의 출결 상태 변경을 위한 버튼을 추가했습니다.
    - 보강의 경우, 날짜 및 시간을 선택할 수 있도록 구현했습니다.

--- src/pages/schedule/calendar/body/LessonListModal/AttendanceButtonList.tsx
+++ src/pages/schedule/calendar/body/LessonListModal/AttendanceButtonList.tsx
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 import { ATTENDANCE_TYPE } from './types';
 import { ATTENDANCE_COLORS } from '@/constants/lesson';
+import NormalButton from '@/shared/button/NormalButton';
+import { useDateModalStore } from '@/store/dateModalStore';
+import { useTimeModalStore } from '@/store/timeModalStore';
+import CalendarModal from '@/shared/form/CalendarModal';
+import TimeModal from '@/shared/form/TimeModal';
 
 interface AttendanceButtonListProps {
   attendanceStatus: string | null;
@@ -11,27 +16,54 @@ export default function AttendanceButtonList({
 }: AttendanceButtonListProps) {
   const [status, setStatus] = useState(attendanceStatus);
 
-  return (
-    <div className="flex gap-2">
-      {Array.from(ATTENDANCE_TYPE.entries()).map(([key, label]) => {
-        const color = ATTENDANCE_COLORS.get(key) ?? '#E5E7EB';
-        const isActive = status === key;
+  const {
+    isOpen: isOpenDate,
+    open: openDate,
+    setSelectedDate,
+  } = useDateModalStore();
+  const { isOpen: isOpenTime, open: openTime } = useTimeModalStore();
 
-        return (
-          <button
-            key={String(key)}
-            onClick={() => setStatus(key)}
-            style={{
-              borderColor: color,
-              backgroundColor: isActive ? color : '#FFFFFF',
-              color: isActive ? '#FFFFFF' : color,
-            }}
-            className="px-3 border transition-colors rounded-2xl"
-          >
-            {label}
-          </button>
-        );
-      })}
+  const handleSaveStatus = () => {
+    if (status === 'makeup') {
+      openDate();
+    }
+  };
+
+  const handleDateSelect = (date: string) => {
+    setSelectedDate(date); // 날짜 저장
+    openTime(); // 시간 모달 열기
+  };
+
+  return (
+    <div className="flex justify-between">
+      <div className="flex gap-2">
+        {Array.from(ATTENDANCE_TYPE.entries()).map(([key, label]) => {
+          const color = ATTENDANCE_COLORS.get(key) ?? '#E5E7EB';
+          const isActive = status === key;
+
+          return (
+            <button
+              key={String(key)}
+              onClick={() => setStatus(key)}
+              style={{
+                borderColor: color,
+                backgroundColor: isActive ? color : '#FFFFFF',
+                color: isActive ? '#FFFFFF' : color,
+              }}
+              className="px-3 border transition-colors rounded-2xl"
+            >
+              {label}
+            </button>
+          );
+        })}
+      </div>
+      <NormalButton
+        text="저장"
+        onClick={handleSaveStatus}
+        disabled={attendanceStatus === status}
+      />
+      {isOpenDate && <CalendarModal onSelect={handleDateSelect} />}
+      {isOpenTime && <TimeModal />}
     </div>
   );
 }

5ac70baba5f7a85366747b15313743765720e397
pangil5634 <oksk6681@naver.com>
2026-02-26 02:08:48 +0900

    chore: remove lesson modal debug log
    레슨 목록 모달에서 디버그 로그를 제거했습니다.

--- src/pages/schedule/calendar/body/LessonListModal/LessonListModal.tsx
+++ src/pages/schedule/calendar/body/LessonListModal/LessonListModal.tsx
@@ -31,7 +31,6 @@ export default function LessonListModal({
     }
 
     const day = dataMap[selectedDate];
-    console.log(day?.lessons);
     setLessons(day ? day.lessons : []);
   }, [dataMap, selectedDate]);
   return (

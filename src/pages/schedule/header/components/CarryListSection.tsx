22a105e58cbddd267df526629bf388f5bf48d41b
pangil5634 <oksk6681@naver.com>
2026-02-27 08:42:03 +0900

    refactor: add carry list section entry
    이월 목록 섹션 컴포넌트를 별도 파일로 분리했습니다.
    
    헤더 구성 요소를 파일 단위로 정리해 가독성을 높였습니다.

---
+++ src/pages/schedule/header/components/CarryListSection.tsx
@@ -0,0 +1,22 @@
+import NormalButton from '@/shared/button/NormalButton';
+import { useCarryModalStore } from '@/store/carryModalStore';
+import CarryListModal from './CarryListSection/components/CarryListModal';
+
+interface CarryListSectionProps {
+  onRefreshLessons: () => Promise<void>;
+}
+
+// 이월 목록 버튼과 모달을 포함하는 섹션 컴포넌트
+function CarryListSection({ onRefreshLessons }: CarryListSectionProps) {
+  const { open, isOpen } = useCarryModalStore(); // 이월 목록 모달의 열림 상태와 열기 함수를 스토어에서 가져옴
+  return (
+    <div>
+      {/* 1. 이월 목록 버튼 */}
+      <NormalButton text="이월 목록" onClick={open} />
+
+      {/* 2. 이월 목록 모달 */}
+      {isOpen && <CarryListModal onRefreshLessons={onRefreshLessons} />}
+    </div>
+  );
+}
+export default CarryListSection;

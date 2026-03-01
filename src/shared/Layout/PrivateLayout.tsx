b0ed52be6df97a80ca958a95cbe6f6598f8baef1
livingdavid <livingdavid@naver.com>
2026-03-01 12:19:01 +0900

    feat: add message send modal infrastructure
    문자 전송 모달과 전용 스토어를 추가했습니다.
    
    PrivateLayout에 모달을 마운트해 전역에서 열 수 있게 했습니다.

--- src/shared/Layout/PrivateLayout.tsx
+++ src/shared/Layout/PrivateLayout.tsx
@@ -1,12 +1,14 @@
 import ToastContainer from '../toast/ToastContainer';
 import OutletLayout from './OutletLayout';
 import PrivateSidebar from './PrivateSidebar';
+import MessageSendModal from '@/shared/message/MessageSendModal';
 
 function PrivateLayout() {
   return (
     <div className="w-full flex min-h-screen flex-col md:flex-row">
       <PrivateSidebar />
       <OutletLayout />
+      <MessageSendModal />
       <ToastContainer />
     </div>
   );
